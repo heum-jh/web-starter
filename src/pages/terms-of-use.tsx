254735e5c9f61339c024ca6cd90d00764ec42540
taehwajangwavle <taehwa.jang@wavle.io>
2023-10-06 14:17:55 +0900

    fix: className 공백제거 및 input check 형식 수정

--- src/pages/terms-of-use.tsx
+++ src/pages/terms-of-use.tsx
@@ -1,33 +1,28 @@
 import clsx from "clsx";
 import Image from "next/image";
-import { useCallback, useEffect, useState } from "react";
-
+import { ChangeEvent, useState } from "react";
+const currentCheckedState = {
+  overAge: false,
+  service: false,
+  agreeInfo: false,
+  marketing: false,
+} as const;
 const ServicePage = () => {
-  const [allcheck, setAllcheck] = useState<boolean>(false);
-  const [overAge, setOverAge] = useState<boolean>(false);
-  const [service, setService] = useState<boolean>(false);
-  const [agreeInfo, setAgreeInfo] = useState<boolean>(false);
-  const [marketing, setMarketing] = useState<boolean>(false);
-  useEffect(() => {
-    if (allcheck) {
-      setOverAge(true);
-      setService(true);
-      setAgreeInfo(true);
-      setMarketing(true);
-    } else {
-      setOverAge(false);
-      setService(false);
-      setAgreeInfo(false);
-      setMarketing(false);
+  const [checkedState, setCheckedState] = useState<typeof currentCheckedState & { [key: string]: boolean }>(
+    currentCheckedState,
+  );
+  const handleOnChange = (target: string) => {
+    setCheckedState(prev => ({ ...prev, [target]: !checkedState[target] }));
+  };
+  const handleSelectAll = (event: ChangeEvent<HTMLInputElement>) => {
+    const updatedCheckedState: { [key: string]: boolean } = {};
+    for (const key in checkedState) {
+      if (checkedState.hasOwnProperty(key)) {
+        updatedCheckedState[key] = event.target.checked;
+      }
     }
-  }, [allcheck]);
-  const allcheckState = useCallback(() => {
-    if (overAge && service && agreeInfo && marketing) {
-      return true;
-    } else {
-      return false;
-    }
-  }, [agreeInfo, marketing, overAge, service]);
+    setCheckedState(updatedCheckedState as typeof currentCheckedState);
+  };
   return (
     <div className="mt-6 px-5">
       <div>
@@ -42,11 +37,17 @@ const ServicePage = () => {
       <div>
         <label className="flex h-14 w-full cursor-pointer items-center justify-between rounded-[0.25rem] bg-[#F4F4F5] px-5 text-[1.125rem]/[1.5rem] font-semibold">
           <span>전체동의</span>
-          <input type="checkbox" className="hidden" checked={allcheckState()} onChange={() => setAllcheck(!allcheck)} />
+          <input
+            type="checkbox"
+            name="agree"
+            className="hidden"
+            checked={Object.values(checkedState).every(value => value === true)}
+            onChange={handleSelectAll}
+          />
           <div
             className={clsx(
-              "flex h-5 w-5 items-center justify-center rounded-full ",
-              allcheckState() ? "bg-[#FF7314]" : "bg-[#DBDEE4]",
+              "flex h-5 w-5 items-center justify-center rounded-full",
+              Object.values(checkedState).every(value => value === true) ? "bg-[#FF7314]" : "bg-[#DBDEE4]",
             )}
           >
             <div className="relative h-[0.375rem] w-[0.563rem]">
@@ -59,11 +60,18 @@ const ServicePage = () => {
             <span>
               <span className="font-medium">[필수]</span> 만 14세 이상
             </span>
-            <input type="checkbox" className="hidden" checked={overAge} onChange={() => setOverAge(!overAge)} />
+            <input
+              type="checkbox"
+              name="agree"
+              value="overAge"
+              className="hidden"
+              checked={checkedState.overAge}
+              onChange={() => handleOnChange("overAge")}
+            />
             <div
               className={clsx(
-                "flex h-5 w-5 items-center justify-center rounded-full ",
-                overAge ? "bg-[#FF7314]" : "bg-[#DBDEE4]",
+                "flex h-5 w-5 items-center justify-center rounded-full",
+                checkedState.overAge ? "bg-[#FF7314]" : "bg-[#DBDEE4]",
               )}
             >
               <div className="relative h-[0.375rem] w-[0.563rem]">
@@ -71,15 +79,22 @@ const ServicePage = () => {
               </div>
             </div>
           </label>
-          <label className="flex h-16 w-full cursor-pointer items-center  justify-between px-5">
+          <label className="flex h-16 w-full cursor-pointer items-center justify-between px-5">
             <span>
               <span className="font-medium">[필수]</span> 서비스 이용약관
             </span>
-            <input type="checkbox" className="hidden" checked={service} onChange={() => setService(!service)} />
+            <input
+              type="checkbox"
+              name="agree"
+              value="service"
+              className="hidden"
+              checked={checkedState.service}
+              onChange={() => handleOnChange("service")}
+            />
             <div
               className={clsx(
                 "flex h-5 w-5 items-center justify-center rounded-full ",
-                service ? "bg-[#FF7314]" : "bg-[#DBDEE4]",
+                checkedState.service ? "bg-[#FF7314]" : "bg-[#DBDEE4]",
               )}
             >
               <div className="relative h-[0.375rem] w-[0.563rem]">
@@ -87,15 +102,22 @@ const ServicePage = () => {
               </div>
             </div>
           </label>
-          <label className="flex h-16 w-full cursor-pointer items-center  justify-between px-5">
+          <label className="flex h-16 w-full cursor-pointer items-center justify-between px-5">
             <span>
               <span className="font-medium">[필수]</span> 개인정보 제3자 제공 동의
             </span>
-            <input type="checkbox" className="hidden" checked={agreeInfo} onChange={() => setAgreeInfo(!agreeInfo)} />
+            <input
+              type="checkbox"
+              name="agree"
+              value="agreeInfo"
+              className="hidden"
+              checked={checkedState.agreeInfo}
+              onChange={() => handleOnChange("agreeInfo")}
+            />
             <div
               className={clsx(
-                "flex h-5 w-5 items-center justify-center rounded-full ",
-                agreeInfo ? "bg-[#FF7314]" : "bg-[#DBDEE4]",
+                "flex h-5 w-5 items-center justify-center rounded-full",
+                checkedState.agreeInfo ? "bg-[#FF7314]" : "bg-[#DBDEE4]",
               )}
             >
               <div className="relative h-[0.375rem] w-[0.563rem]">
@@ -103,15 +125,22 @@ const ServicePage = () => {
               </div>
             </div>
           </label>
-          <label className="flex h-16 w-full cursor-pointer items-center  justify-between px-5">
+          <label className="flex h-16 w-full cursor-pointer items-center justify-between px-5">
             <span>
               <span className="font-medium">[선택]</span> 마케팅 정보 수신동의
             </span>
-            <input type="checkbox" className="hidden" checked={marketing} onChange={() => setMarketing(!marketing)} />
+            <input
+              type="checkbox"
+              name="agree"
+              value="marketing"
+              className="hidden"
+              checked={checkedState.marketing}
+              onChange={() => handleOnChange("marketing")}
+            />
             <div
               className={clsx(
-                "flex h-5 w-5 items-center justify-center rounded-full ",
-                marketing ? "bg-[#FF7314]" : "bg-[#DBDEE4]",
+                "flex h-5 w-5 items-center justify-center rounded-full",
+                checkedState.marketing ? "bg-[#FF7314]" : "bg-[#DBDEE4]",
               )}
             >
               <div className="relative h-[0.375rem] w-[0.563rem]">
@@ -124,7 +153,7 @@ const ServicePage = () => {
       <button
         className={clsx(
           "mt-3 h-14 w-full cursor-pointer rounded-[0.25rem] bg-[#FDDCB0] text-[1.125rem]/[1.5rem] font-semibold text-[#ffffff]",
-          (overAge && service && agreeInfo) || allcheck ? "bg-[#FF7314]" : "bg-[#FDDCB0]",
+          checkedState.agreeInfo && checkedState.overAge && checkedState.service ? "bg-[#FF7314]" : "bg-[#FDDCB0]",
         )}
       >
         동의하기
