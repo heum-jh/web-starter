764666ae2d85ce4e54dc226b848a223d96948d78
lee gilhak <lgh@yumis.io>
2023-10-11 11:47:35 +0900

    fix: stroke관련 props 수정

--- src/pages/mypage/setting.tsx
+++ src/pages/mypage/setting.tsx
@@ -7,19 +7,19 @@ const MyPageSettingPage: NextPageWithLayout = () => {
         <li className="flex h-[4rem] items-center justify-between border-b border-[#F0F1F2] bg-[#ffffff] pl-[1.25rem] pr-[1.25rem] ">
           <div className=" text-lg/[1.5rem] font-medium">공지사항</div>
           <svg width="5" height="8" viewBox="0 0 5 8" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M1 7L4 4L1 1" stroke="#707888" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+            <path d="M1 7L4 4L1 1" stroke="#707888" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
           </svg>
         </li>
         <li className="flex h-[4rem] items-center justify-between border-b border-[#F0F1F2] bg-[#ffffff] pl-[1.25rem] pr-[1.25rem] ">
           <div className=" text-lg/[1.5rem] font-medium">자주 묻는 질문</div>
           <svg width="5" height="8" viewBox="0 0 5 8" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M1 7L4 4L1 1" stroke="#707888" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+            <path d="M1 7L4 4L1 1" stroke="#707888" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
           </svg>
         </li>
         <li className="flex h-[4rem] items-center justify-between border-b border-[#F0F1F2] bg-[#ffffff] pl-[1.25rem] pr-[1.25rem] ">
           <div className=" text-lg/[1.5rem] font-medium">1:1 문의</div>
           <svg width="5" height="8" viewBox="0 0 5 8" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M1 7L4 4L1 1" stroke="#707888" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+            <path d="M1 7L4 4L1 1" stroke="#707888" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
           </svg>
         </li>
       </ul>
@@ -27,13 +27,13 @@ const MyPageSettingPage: NextPageWithLayout = () => {
         <li className="flex h-[4rem] items-center justify-between border-b border-[#F0F1F2] bg-[#ffffff] pl-[1.25rem] pr-[1.25rem] ">
           <div className=" text-lg/[1.5rem] font-medium">계정 관리</div>
           <svg width="5" height="8" viewBox="0 0 5 8" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M1 7L4 4L1 1" stroke="#707888" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+            <path d="M1 7L4 4L1 1" stroke="#707888" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
           </svg>
         </li>
         <li className="flex h-[4rem] items-center justify-between border-b border-[#F0F1F2] bg-[#ffffff] pl-[1.25rem] pr-[1.25rem] ">
           <div className=" text-lg/[1.5rem] font-medium">알림 설정</div>
           <svg width="5" height="8" viewBox="0 0 5 8" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M1 7L4 4L1 1" stroke="#707888" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+            <path d="M1 7L4 4L1 1" stroke="#707888" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
           </svg>
         </li>
       </ul>
@@ -41,7 +41,7 @@ const MyPageSettingPage: NextPageWithLayout = () => {
         <li className="flex h-[4rem] items-center justify-between  bg-[#ffffff] pl-[1.25rem] pr-[1.25rem] ">
           <div className=" text-lg/[1.5rem] font-medium">이용약관</div>
           <svg width="5" height="8" viewBox="0 0 5 8" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M1 7L4 4L1 1" stroke="#707888" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+            <path d="M1 7L4 4L1 1" stroke="#707888" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
           </svg>
         </li>
       </ul>
