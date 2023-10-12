0322a53a602c5e5c576741c50952fc6caecba687
heum-jh <gihum@wavle.io>
2023-10-12 10:49:22 +0900

    fix: sort 컴포넌트 추가

--- src/pages/article/index.tsx
+++ src/pages/article/index.tsx
@@ -1,15 +1,25 @@
 import { useRouter } from "next/router";
 import ArticleCard from "src/components/article/article-card";
 import { NextPageWithLayout } from "../_app";
+import SelectPopup from "src/components/common/select-popup";
+import { useState } from "react";
 
 const ArticlePage: NextPageWithLayout = () => {
   const router = useRouter();
+  const [sort, setSort] = useState("new");
   return (
     <main className="container">
       <h1 className="px-5 py-[0.94rem] font-SUITE text-2xl font-extrabold text-[#111111]">아티클</h1>
       <div className="flex items-center justify-between px-5 pb-[0.63rem] pt-3">
         <span className="text-base font-semibold text-[#111111]">00개의 발행</span>
-        <button>TEST</button>
+        <SelectPopup
+          list={[
+            { label: "최신순", value: "new" },
+            { label: "좋아요 순", value: "like" },
+          ]}
+          value={sort}
+          onChange={setSort}
+        />
       </div>
       <div>
         <ul className="divide-y divide-[#F0F1F2]">
